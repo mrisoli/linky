9164189a2541a35c9167647556d47b68ed701688
Marcelo Risoli <celorisoli@gmail.com>
2022-03-09 15:08:23 -0600

    add tailwind (#6)

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@
 /// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import sveltePreprocess from 'svelte-preprocess'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte({ hot: !process.env.VITEST })],
+  plugins: [svelte({
+    hot: !process.env.VITEST,
+    preprocess: sveltePreprocess()
+  })],
   test: {
     globals: true,
     environment: 'jsdom'
