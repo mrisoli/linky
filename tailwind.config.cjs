9ffef237d4d581b9684f08fcd7f1e9512d98f8d9
Marcelo Risoli <celorisoli@gmail.com>
2022-03-09 15:03:58 -0600

    add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  plugins: [
+  ],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+};
