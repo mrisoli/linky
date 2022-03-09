296e2600d736e3a66c0f4a0156f1f71ce8ef1579
Marcelo Risoli <celorisoli@gmail.com>
2022-03-09 14:01:00 -0600

    add test

---
+++ src/App.test.ts
@@ -0,0 +1,10 @@
+import { expect, test } from 'vitest'
+import { render } from '@testing-library/svelte'
+
+import App from './App.svelte'
+
+test('shows proper heading when rendered', () => {
+  const {getByText} = render(App)
+
+  expect(getByText('Hello Typescript!')).toBeInTheDocument()
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte({ hot: !process.env.VITEST })],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './other/setup-test-env'
+  },
 })
