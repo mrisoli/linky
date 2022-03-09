edaaf3a37fa9110fcc73990ee6eef451e713447b
Marcelo Risoli <celorisoli@gmail.com>
2022-03-09 14:25:46 -0600

    add eslint and prettier

--- src/App.test.ts
+++ src/App.test.ts
@@ -4,7 +4,7 @@ import { render } from '@testing-library/svelte'
 import App from './App.svelte'
 
 test('shows proper heading when rendered', () => {
-  const {getByText} = render(App)
+  const { getByText } = render(App)
 
   expect(getByText('Hello Typescript!')).toBeTruthy()
 })
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,5 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom'
-  },
+  }
 })
